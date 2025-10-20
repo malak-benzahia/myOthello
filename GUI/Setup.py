cd187e0ba9eb52678d1f27b5f754f8e5a6851f94
malak-benzahia <malakbenzahia@gmail.com>
2025-10-20 19:41:09 +0100

    fixed the gui and added new featues and fixed some bugs

--- GUI/Setup.py
+++ GUI/Setup.py
@@ -1,17 +1,34 @@
 import pygame as pg
 from pygame.locals import *
-import time
+from Logic.Board import Board
 import sys
+import time
 
 pg.init()
 
 class Game:
+    # General variables of the game
     SCREEN_WIDTH = 1200
     SCREEN_HEIGHT = 700 
     CELL_SIZE = 60
     BOARD_SIZE = 8
     BOARD_TOPLEFT = (100,100)
+    board = Board()
+    
 
+    def __init__(self):
+        self.screen = pg.display.set_mode((Game.SCREEN_WIDTH, 
+                                           Game.SCREEN_HEIGHT))
+        pg.display.set_caption("myOthello")
+    
+        self.running = True
+        self.startTime = pg.time.get_ticks()
+
+        self.turn = self.board.BLACK
+
+        self.last_move = -1, -1
+    
+    # Drawing the initial empty board
     def drawBoard(self):
         for row in range(self.BOARD_SIZE):
             for col in range(self.BOARD_SIZE):
@@ -20,7 +37,17 @@ class Game:
                                self.CELL_SIZE, self.CELL_SIZE)
                 pg.draw.rect(self.screen, (0, 100, 0), rect)
                 pg.draw.rect(self.screen, (0, 0, 0), rect, 1)
-
+    # redrawing board with the correct discs           
+    def redrawBoard(self):
+        self.announceWinner()
+        self.piecesTracking()
+        self.highlightPossibleMoves()
+        for row in range(self.BOARD_SIZE):
+                for col in range(self.BOARD_SIZE):
+                    if self.board.board[row, col] == self.board.BLACK:
+                        self.drawDisc(self.board.BLACK, self.findSquareTopleftCoordsByIndex((row, col)))
+                    elif self.board.board[row, col] == self.board.WHITE:
+                        self.drawDisc(self.board.WHITE, self.findSquareTopleftCoordsByIndex((row,col)))
     # drawing the discs 
     def drawDisc(self, color: int, coords: tuple[int, int]) -> None:
         center = (coords[0] + self.CELL_SIZE // 2, coords[1] + self.CELL_SIZE // 2)
@@ -28,63 +55,140 @@ class Game:
             pg.draw.circle(self.screen, (0, 0, 0), center, 26)
         else: # 0 for white disc
             pg.draw.circle(self.screen, (255, 255, 255), center, 26)
-
-
     # hover effect 
     def hover(self) -> None:
         mx, my = pg.mouse.get_pos()
-        square = self.findSquare((mx, my))
+        square = self.findSquareTopleftCoords((mx, my))
+        index = self.getSquareIndex((mx, my))
 
-        if square:
+        if square and index:
             x, y = square
-            if (1, square) not in self.discs and (0, square) not in self.discs:
+            row, col = index   # unpack the grid coordinates
+            if self.board.board[row, col] == 0:  # empty cell
                 rect = pg.Rect(x, y, self.CELL_SIZE, self.CELL_SIZE)
-                pg.draw.rect(self.screen, (0, 170, 0), rect, 60, 10)
+                pg.draw.rect(self.screen, (0, 170, 0), rect, 4)
+    # finding top left coords of a square in the board using any coords in the square
+    def findSquareTopleftCoords(self, coords: tuple[int, int]) -> tuple[int, int] | None:
+        square = self.getSquareIndex(coords)
+        if square is None:
+            return None  # mouse outside board
 
-        
+        col, row = square
+        c = self.BOARD_TOPLEFT[0] + col * self.CELL_SIZE
+        r = self.BOARD_TOPLEFT[1] + row * self.CELL_SIZE
+        return (c, r)
+    # finding top left coords using the index of the square
+    def findSquareTopleftCoordsByIndex(self, index: tuple[int, int]) -> tuple[int, int] | None:
+        row, col= index
+        x, y = self.BOARD_TOPLEFT
 
+        c = self.BOARD_TOPLEFT[0] + col*self.CELL_SIZE
+        r = self.BOARD_TOPLEFT[1] + row*self.CELL_SIZE
 
-    def findSquare(self, coords: tuple[int, int]) -> tuple[int, int] | None:
+        return (r, c)  
+    # getting the index of a square using coords inside it
+    def getSquareIndex(self, coords: tuple[int, int]):
         mx, my = coords
         x, y = self.BOARD_TOPLEFT
 
-        if (mx< x or mx>x+8*self.CELL_SIZE) or (my<y or my>y+8*self.CELL_SIZE):
+        if (mx< x or mx>=x+8*self.CELL_SIZE) or (my<y or my>=y+8*self.CELL_SIZE):
             return 
         
         col = (mx - self.BOARD_TOPLEFT[0]) // self.CELL_SIZE
         row = (my - self.BOARD_TOPLEFT[1]) // self.CELL_SIZE
 
-        c = self.BOARD_TOPLEFT[0] + col*self.CELL_SIZE
-        r = self.BOARD_TOPLEFT[1] + row*self.CELL_SIZE
-
-        return (c, r)
-    
-
-
-    def __init__(self):
-        self.screen = pg.display.set_mode((Game.SCREEN_WIDTH, 
-                                           Game.SCREEN_HEIGHT))
-        pg.display.set_caption("myOthello")
-    
-        self.running = True
-        
-        self.turn = 1  # 1 = black, 0 = white
-        # store discs as (color, x, y)
-        self.discs = []
-
-
-
+        return(col, row)
+    # handling clicking events 
     def handleMouseClick(self) -> None:
         mx, my = pg.mouse.get_pos()
-        square = self.findSquare((mx, my))
-
+        square = self.getSquareIndex((mx, my))
+        moves = self.board.findAllPossibleMoves(self.turn)
         if square:
-            self.discs.append((self.turn, square))
-            self.turn = 1 - self.turn 
+            row, col = square
+            if self.board.board[row, col] ==0 and (row, col) in moves:
+                self.board.board[row, col] = self.turn
+                self.board.setDiscs(row, col, self.turn)
+                self.turn = -1 * self.turn
+                self.last_move =(col, row)
+               
+    def announceWinner(self):
+        if self.board.isGameOver() is True:
+                my_font = pg.font.SysFont('Arial', 30) 
+                text_surface = my_font.render('Game ended', True, (255, 255, 255)) 
+                # Text, Antialiasing, Color (RGB)
+                self.screen.blit(text_surface, (700, 500)) # Text surface, Position (x, y)
 
+                if self.board.white_disc_count > self.board.black_disc_count:
+                    winner = my_font.render('White won!', True, (255, 255, 255))
+                    self.screen.blit(winner, (700, 300))
+                elif self.board.white_disc_count < self.board.black_disc_count:
+                    winner = my_font.render('Black won!', True, (255, 255, 255))
+                    self.screen.blit(winner, (700, 300))
+                else:
+                    winner = my_font.render('it is a tie!', True, (255, 255, 255))
+                    self.screen.blit(winner, (700, 300))
 
-    def start(self) -> None:
-        
+    def highlightPossibleMoves(self):
+        moves = self.board.findAllPossibleMoves(self.turn)
+
+        overlay = pg.Surface(self.screen.get_size(), pg.SRCALPHA)
+
+        for move in moves: 
+            row, col = move
+            x, y = self.findSquareTopleftCoordsByIndex((row, col))
+            center = (x + self.CELL_SIZE // 2, y + self.CELL_SIZE // 2)
+
+            if self.turn == 1:  # 1 for black 
+                pg.draw.circle(overlay, (0, 0, 0, 60), center, 26)
+            else:  # 0 for white 
+                pg.draw.circle(overlay, (255, 255, 255, 100), center, 26)
+
+        self.screen.blit(overlay, (0, 0))
+
+    def piecesTracking(self):
+        my_font = pg.font.SysFont('Arial', 30) 
+        white_num = self.board.whiteDiscCount()
+        black_num = self.board.blackDiscCount()
+        white = my_font.render(f"White : {white_num}", True, (255, 255, 255)) 
+        black = my_font.render(f"Black : {black_num}", True, (255, 255, 255))
+        self.screen.blit(white, (700, 150))
+        self.screen.blit(black, (700, 100))
+
+    def timeLapse(self):
+        elapsed_ms = pg.time.get_ticks() - self.startTime
+        elapsed_sec = elapsed_ms // 1000
+        minutes = elapsed_sec // 60 
+        seconds = elapsed_sec % 60
+
+        my_font = pg.font.SysFont('Arial', 24)
+        text_surface = my_font.render(f"Time: {minutes:02}:{seconds:02}", True, (255, 255, 255))
+        self.screen.blit(text_surface, (700, 50))
+
+    def currentTurn(self):
+        if self.turn == self.board.BLACK:
+            current = "Black"
+        else:
+            current = "White"
+        my_font = pg.font.SysFont('Arial', 24)
+        text_surface = my_font.render(f"Current Turn: {current}", True, (255, 255, 255))
+        self.screen.blit(text_surface, (700, 200))
+
+    def lastMove(self):
+        lastr, lastc = self.last_move
+        if lastr == -1 and lastc == -1:
+            return
+        my_font = pg.font.SysFont('Arial', 24)
+        text_surface = my_font.render(f"last Turn: ({lastr+1}, {lastc+1})", True, (255, 255, 255))
+        self.screen.blit(text_surface, (700, 300))
+
+    #def chooseRole(self):
+        #to implement
+
+    #def chooseMatchType(self):
+        #to implement
+
+    
+    def start(self) -> None:        
         while self.running :
             for event in pg.event.get():
                 if event.type == QUIT:
@@ -96,10 +200,11 @@ class Game:
             self.screen.fill((50, 50, 50))      # background color
             self.drawBoard()
 
-             # draw all discs stored
-            for color, pos in self.discs:
-                self.drawDisc(color, pos)                  # draw grid
+            self.redrawBoard()
+            self.currentTurn()
             self.hover()
+            self.timeLapse()
+            self.lastMove()
             pg.display.flip() 
     
 
---
+++ Logic/Board.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+class Board:
+    WHITE = -1
+    BLACK = 1
+    EMPTY = 0
+
+    moves = [( 0,-1),    #top
+             ( 0, 1),    #bottom
+             ( 1, 0),    #right
+             (-1, 0),    #left
+             ( 1,-1),    #top right
+             ( 1, 1),    #bottom right
+             (-1, 1),    #bottom left
+             (-1,-1),    #top left
+    ]
+    
+    def __init__(self):
+        
+        self.board = np.array([0]*8, dtype=np.int64)
+        self.board = self.board[np.newaxis, :]
+        for _ in range(3):
+            self.board = np.concatenate((self.board, self.board), axis = 0)
+        
+        self.board[3, 3] = self.board[4,4] = Board.WHITE
+        self.board[3, 4] = self.board[4,3] = Board.BLACK
+
+        self.black_disc_count = 2
+        self.white_disc_count = 2
+
+    def checkBounds(self, x: int, y: int) -> bool:
+        return (x >= 0 and y >= 0) and (x < 8 and y < 8)
+    
+    def findPossibleMoves(self, r: int, c: int):
+        moves = []
+
+        player = self.board[r, c]
+        opp = player * -1
+
+        if player == 0:  # empty
+            return moves
+        
+        for dir in self.moves:
+            rowDir, colDir = dir
+            row = r + rowDir
+            col = c + colDir
+
+            if self.checkBounds(row, col) and self.board[row, col] == opp:
+                while(self.checkBounds(row, col) and self.board[row, col] == opp):
+                    row += rowDir
+                    col += colDir
+
+                if (self.checkBounds(row, col) and self.board[row, col] == Board.EMPTY):
+                    moves.append((row, col))
+
+                else:
+                    continue
+            else: 
+                continue
+        return moves
+
+    def findAllPossibleMoves(self, player: int):
+        allMoves = set()
+        for r in range(self.board.shape[0]):      # iterate over row indices
+            for c in range(self.board.shape[1]):  # iterate over column indices
+                if self.board[r, c] == player:
+                    moves = self.findPossibleMoves(r, c)
+                    allMoves.update(moves)
+        return allMoves
+    
+    def flipDiscs(self, start: tuple[int, int], end: tuple[int, int], player: int, dir: tuple[int, int]):
+        row, col = start
+        rowDir, colDir = dir
+
+        row += rowDir
+        col += colDir
+
+        while (row, col) != end:
+            self.board[row, col] = player
+            if player == self.BLACK:
+                self.black_disc_count += 1
+            elif player == self.WHITE:
+                self.white_disc_count += 1
+
+            row += rowDir
+            col += colDir
+
+    def setDiscs(self, row: int, col: int, player: int):
+        self.board[row, col] = player
+        opp = player * -1
+
+        for dir in Board.moves:
+            rowDir, colDir = dir
+            r = row + rowDir
+            c = col + colDir
+
+            if self.checkBounds(r, c) is False or self.board[r, c] != opp:
+                continue
+
+            r += rowDir 
+            c += colDir
+            while(self.checkBounds(r, c) is True and self.board[r, c] == opp):
+                r += rowDir
+                c += colDir
+            if(self.checkBounds(r, c) is True and self.board[r, c] == player):
+                self.flipDiscs((row, col), (r, c), player, dir)
+
+        
+    def isGameOver(self) -> bool:
+        if not self.findAllPossibleMoves(self.BLACK) and not self.findAllPossibleMoves(self.WHITE):
+            return True
+        return False
+    
+    def blackDiscCount(self):
+        count = 0
+        for row in self.board:
+            for cell in row:
+                if cell == self.BLACK:
+                    count += 1
+        return count
+    
+    def whiteDiscCount(self):
+        count = 0
+        for row in self.board:
+            for cell in row:
+                if cell == self.WHITE:
+                    count += 1
+        return count
+    
+    def getWinner(self):
+        if self.white_disc_count > self.black_disc_count:
+            return self.WHITE
+        elif self.black_disc_count > self.white_disc_count:
+            return self.BLACK
+        else :
+            return 0
+
